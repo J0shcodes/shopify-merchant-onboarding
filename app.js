21bc92d945b24a69a5bc165de31bf5148c5b110d
Joshua Moses <mosesjoshua350@gmail.com>
2023-11-27 09:19:55 +0100

    Add javascript features and functionalities

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const notifBell = document.getElementById("bell");
 const profileButton = document.getElementById("merchant");
-const alertModal = document.getElementById("alert-container");
+const alertModal = document.querySelector(".alert-container");
 const profileModal = document.querySelector(".profile-modal");
 
 notifBell.addEventListener("click", () => {
-  console.log(profileModal);
-  // alertModal.style.setProperty("display", "inline-flex")
-});
+    console.log("notification")
+  if (alertModal.classList.contains("hide")) {
+    alertModal.classList.remove("hide");
+  } else {
+    alertModal.classList.add("hide");
+  }
 
-profileButton.addEventListener("click", () => {
-  console.log(profileModal);
-  if (profileModal.classList.contains("hide")) {
-    profileModal.classList.remove("hide");
+  if(profileModal.classList.contains("hide")) {
+    return null
   } else {
     profileModal.classList.add("hide")
   }
 });
+
+profileButton.addEventListener("click", () => {
+  if (profileModal.classList.contains("hide")) {
+    profileModal.classList.remove("hide");
+  } else {
+    profileModal.classList.add("hide");
+  }
+
+  if(alertModal.classList.contains("hide")) {
+    return null
+  } else {
+    alertModal.classList.add("hide")
+  }
+});
+
+alertModal.addEventListener("click", () => {
+    alertModal.classList.add("hide")
+})
+
+profileModal.addEventListener("click", () => {
+    profileModal.classList.add("hide")
+})
 console.log(notifBell, profileButton);
